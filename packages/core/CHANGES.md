c0d0aab608e0a20efef36c93ccdada1dc27079fe
huytrongnguyen <huynguyen8505@gmail.com>
2019-04-06 11:10:35 +0700

    [#1] Cannot read property 'message' of undefined

--- packages/core/CHANGES.md
+++ packages/core/CHANGES.md
@@ -1,5 +1,12 @@
 # Roxie Core
 
+## [0.9.4]
+> Apr 6, 2019
+
+### Bug fixed
+
+* [#1] Cannot read property 'message' of undefined
+
 ## [0.9.3]
 > Mar 31, 2019
 
--- packages/core/src/logger.ts
+++ packages/core/src/logger.ts
@@ -2,4 +2,4 @@ import { nowAsString } from './date';
 
 export const logInfo = (msg: string) => console.log(`${nowAsString()} [INFO] ${msg}`);
 export const logWarn = (msg: string) => console.warn(`${nowAsString()} [WARN] ${msg}`);
-export const logError = (msg: string, err?: any) => console.trace(`${nowAsString()} [ERROR] ${msg} caused by ${err.message || err}`);
+export const logError = (msg: string) => console.trace(`${nowAsString()} [ERROR] ${msg} caused by:`);
