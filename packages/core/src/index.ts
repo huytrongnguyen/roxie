17cabefe4cc7891599c5c1b88f5aa1e2381c75b1
huytrongnguyen <huynguyen8505@gmail.com>
2019-10-27 19:18:02 +0700

    Supports `@roxie/server` for logInfo, logError

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -2,3 +2,7 @@ export * from './roxie';
 export * from './ajax';
 export * from './observable';
 export * from './data';
+
+export const logInfo = (msg: string) => console.log(`[INFO] ${msg}`);
+export const logWarn = (msg: string) => console.warn(`[WARN] ${msg}`);
+export const logError = (msg: string) => console.trace(`[ERROR] ${msg}`);
